c3c96eee9fc3f5cd9aeb1812acd7654856d27895
kevin FERRANDON <kferrandon@gmail.com>
2022-05-24 16:52:19 +0200

    add bootstrap

--- Views/templates/Categorie/list.tpl
+++ Views/templates/Categorie/list.tpl
@@ -1,15 +1,23 @@
-<h2><a href="index.php?page=categorie&action=add">Ajouter</a></h2>
+<h2><a class="btn btn-primary" href="index.php?page=categorie&action=add">Ajouter</a></h2>
 <p>{$vue.description}<p>    
 <div>
-    <table><thead><tr><td>Libelle</td><td>Action</td></tr></thead>
+    <table class="table"><thead><tr><td>Libelle</td><td>Action</td></tr></thead>
         <tbody>
 {foreach $vue.categories as $cat}
     <tr><td>{$cat.libelle}<td>
-        <td><a href="index.php?page=categorie&action=update&id={$cat.id}">u</a>
-            | <a href="index.php?page=categorie&action=delete&id={$cat.id}">-</a>
+        <td><a class="btn btn-primary" href="index.php?page=categorie&action=update&id={$cat.id}">modification</a>
+            | <a class="btn btn-danger" href="#" onclick="sup({$cat.id});">Suppression</a>
         </td>
     </tr>
 {/foreach}
     </tbody>
     </table>
 </div>
+    <script>
+        function sup(id){
+            if(window.confirm("Voulez vous vraiment supprimer l'élément?")){
+                window.location.replace("index.php?page=categorie&action=delete&id="+id);
+            }
+        };
+        
+    </script>
--- Views/templates_c/5f3b72d21facb6b7140714bcc4de4dd282ca40f4_0.file.list.tpl.php
+++ Views/templates_c/5f3b72d21facb6b7140714bcc4de4dd282ca40f4_0.file.list.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.1.0, created on 2022-05-24 12:40:31
+/* Smarty version 4.1.0, created on 2022-05-24 14:51:25
   from 'D:\laragon\www\demo-mvc\Views\templates\categorie\list.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.1.0',
-  'unifunc' => 'content_628cd23fc95e94_26680709',
+  'unifunc' => 'content_628cf0ed73fba7_81763237',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '5f3b72d21facb6b7140714bcc4de4dd282ca40f4' => 
     array (
       0 => 'D:\\laragon\\www\\demo-mvc\\Views\\templates\\categorie\\list.tpl',
-      1 => 1653395308,
+      1 => 1653403882,
       2 => 'file',
     ),
   ),
@@ -20,12 +20,12 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_628cd23fc95e94_26680709 (Smarty_Internal_Template $_smarty_tpl) {
-?><h2><a href="index.php?page=categorie&action=add">Ajouter</a></h2>
+function content_628cf0ed73fba7_81763237 (Smarty_Internal_Template $_smarty_tpl) {
+?><h2><a class="btn btn-primary" href="index.php?page=categorie&action=add">Ajouter</a></h2>
 <p><?php echo $_smarty_tpl->tpl_vars['vue']->value['description'];?>
 <p>    
 <div>
-    <table><thead><tr><td>Libelle</td><td>Action</td></tr></thead>
+    <table class="table"><thead><tr><td>Libelle</td><td>Action</td></tr></thead>
         <tbody>
 <?php
 $_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['vue']->value['categories'], 'cat');
@@ -35,10 +35,10 @@ $_smarty_tpl->tpl_vars['cat']->do_else = false;
 ?>
     <tr><td><?php echo $_smarty_tpl->tpl_vars['cat']->value['libelle'];?>
 <td>
-        <td><a href="index.php?page=categorie&action=update&id=<?php echo $_smarty_tpl->tpl_vars['cat']->value['id'];?>
-">u</a>
-            | <a href="index.php?page=categorie&action=delete&id=<?php echo $_smarty_tpl->tpl_vars['cat']->value['id'];?>
-">-</a>
+        <td><a class="btn btn-primary" href="index.php?page=categorie&action=update&id=<?php echo $_smarty_tpl->tpl_vars['cat']->value['id'];?>
+">modification</a>
+            | <a class="btn btn-danger" href="#" onclick="sup(<?php echo $_smarty_tpl->tpl_vars['cat']->value['id'];?>
+);">Suppression</a>
         </td>
     </tr>
 <?php
@@ -47,5 +47,15 @@ $_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
     </tbody>
     </table>
 </div>
+    <?php echo '<script'; ?>
+>
+        function sup(id){
+            if(window.confirm("Voulez vous vraiment supprimer l'élément?")){
+                window.location.replace("index.php?page=categorie&action=delete&id="+id);
+            }
+        };
+        
+    <?php echo '</script'; ?>
+>
 <?php }
 }
